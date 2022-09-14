5711807c2da916a7cdfd62796ad4758fcf14107b
Jonathan Bonzali <katbonzali@gmail.com>
2022-09-14 16:24:43 +0000

    update language

--- src/screens/AboutScreen/index.jsx
+++ src/screens/AboutScreen/index.jsx
@@ -6,7 +6,7 @@ import OurTeam from "@/screens/AboutScreen/components/OurTeam";
 export function AboutScreen() {
     return (
         <>
-             <PageHeader objectPosition={'30% 30%'} image={'/images/aboutHeader.jpg'} introHead={'About Us'} introBody={'We are empowering individuals and positively impacting lives.'}/>
+             <PageHeader objectPosition={'30% 30%'} image={'/images/aboutHeader.jpg'} introHead={'About Us'} introBody={'We are empowering individuals and positively impacting lives. We believe in the power of technology and its ability to be a transformative tool for change. \n'}/>
              <AboutIntro/>
              <MissionVision/>
              <OurTeam/>
